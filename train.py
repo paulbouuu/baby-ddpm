2930125896b41950c1bf6bba3c0cea61f0c44465
paulbouuu <paul.bouye2@gmail.com>
2025-12-02 09:00:53 +0100

    rolling avg loss

--- train.py
+++ train.py
@@ -2,7 +2,7 @@ import torch
 import torch.optim as optim
 from datasets import load_dataset
 from torch.utils.data import DataLoader
-from torchvision import transforms, utils
+from torchvision import transforms
 
 from utils import EMA
 from model import UNet
@@ -20,13 +20,14 @@ else:
 
 
 # hyperparameters
-batch_size = 16
+batch_size = 32
 lr = 1e-4
 epochs = 20
 image_size = 64
 num_workers = 0
 sample_steps = 50 # DDIM steps for sampling
 diffusion_steps = 1000 # diffusion steps
+rolling_avg_window = 100
 # -----------------
 
 
@@ -58,12 +59,17 @@ dataloader = DataLoader(
 # model and diffusion
 model = UNet(in_ch=3, out_ch=3, base=64, time_dim=128, diffusion_steps=diffusion_steps).to(device)
 diffusion = Diffusion(T=diffusion_steps).to(device)
+num_params = sum(p.numel() for p in model.parameters() if p.requires_grad)
+print(f"{num_params/1e6:.2f}M parameters")
 
 optimizer = optim.AdamW(model.parameters(), lr=lr)
 
 ema = EMA(model, beta=0.999)
 
 step = 0
+
+losses = []
+print(f"Rolling average window size: {rolling_avg_window}")
 
 # training loop
 for epoch in range(epochs):
@@ -79,13 +85,21 @@ for epoch in range(epochs):
         optimizer.step()
         ema.update(model)
 
-        if step % 100 == 0:
-            print(f"Epoch {epoch}, Step {step}, Loss: {loss.item():.4f}")
+        losses.append(loss.item())
+        rolling_avg = sum(losses[-rolling_avg_window:]) / min(len(losses), rolling_avg_window)
+
+        if step % 200 == 0:
+            print(f"Epoch {epoch}, Step {step}, Loss: {loss.item():.6f}, Rolling avg loss: {rolling_avg:.6f}")
 
         step += 1
 
     # save model checkpoint
-    torch.save(ema.shadow, f"model_epoch{epoch}.pt")
+    torch.save({
+        "ema_model": ema.shadow,
+        "epoch": epoch}, 
+        f"model_epoch{epoch}.pt"
+    )
+
     print(f"Saved checkpoint epoch {epoch}")
 
     torch.mps.empty_cache()
